a1c8c84a1280f02db44e4c609cb3d6f2afe7e45a
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-20 00:57:07 +0330

    add fanction Authenticate and hashing code

--- src/main/java/model/AuthService.java
+++ src/main/java/model/AuthService.java
@@ -1,2 +1,88 @@
-package model;public class AuthService {
+package model;
+
+
+import Data_Base.DBconnection;
+
+import java.sql.*;
+import java.sql.Connection;
+
+// Authntication function
+public class AuthService {
+
+    //    The function of the register and the password
+    public static boolean register(String username , String password) {
+
+        Connection connection = DBconnection.connect(); //When the database is connected, the address and database returns to the desert in it
+
+        String hashedpassword = HashUtil.hashpassword(password); // send tse password to the Hash function for hashed password
+        String Query = "INSERT INTO aunthnticat (username,password)"//query insert username and password
+                +"VALUES (?,?)";
+
+        try{
+
+            PreparedStatement preparedStatement = connection.prepareStatement(Query); //possiblity of entering information in the table
+
+            preparedStatement.setString(1,username); //enter username in tha table
+            preparedStatement.setString(2,hashedpassword); //enter password in the table
+            preparedStatement.executeUpdate(); //update table
+            return true;
+        } catch (SQLException e) {
+            if (e.getMessage().contains("duplicate key value")) {
+                System.out.println("Username already exists.");
+            } else {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    //    The function of the login
+    public static boolean login(String username ,String password) {
+
+        Connection connection = DBconnection.connect();//When the database is connected, the address and database returns to the desert in it
+
+        String hashedpassword = HashUtil.hashpassword(password);// send tse password to the Hash function for hashed password
+        String Query = "SELECT FROM aunthnticat WHERE username = ? AND password = ?"; //query login
+
+        try {
+
+            PreparedStatement preparedStatement  = connection.prepareStatement(Query);//possiblity of entering information in the table
+
+            preparedStatement.setString(1,username);//enter username in tha table
+            preparedStatement.setString(2,hashedpassword);//enter password in the table
+
+            ResultSet resultSet = preparedStatement.executeQuery(); //Returns the number as many rows that change
+            return resultSet.next();
+
+        } catch (SQLException e) {
+            System.out.println("Error : " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean resetPassword(String usernam,String password) {
+
+        Connection connection = DBconnection.connect();//When the database is connected, the address and database returns to the desert in it
+
+        String hashedpassword = HashUtil.hashpassword(password);// send tse password to the Hash function for hashed password
+
+        String Query = "UPDATE aunthnticat SET password = ? WHERE username = ?";//query updata password
+        try {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(Query);//possiblity of entering information in the table
+
+
+            preparedStatement.setString(1,hashedpassword);//enter username in tha table
+            preparedStatement.setString(2,usernam);//enter password in the table
+
+            int affectedRows = preparedStatement.executeUpdate();//Returns the number as many rows that change
+
+            return affectedRows>0;
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
 }
+
--- src/main/java/model/HashUtil.java
+++ src/main/java/model/HashUtil.java
@@ -1,2 +1,24 @@
-package model;public class HashUtil {
+package model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtil {
+    public static String hashpassword(String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256"); //Determine to use SHA-256 to hashed code
+            byte[] hashByte = messageDigest.digest(password.getBytes()); //Convert password to the binery code
+
+            StringBuilder hexString = new StringBuilder();
+
+            for(byte b : hashByte) {
+                hexString.append(String.format("%02x",b)); // convert binery code to the hexacode
+            }
+
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 algorithm not found.");
+        }
+    }
 }
