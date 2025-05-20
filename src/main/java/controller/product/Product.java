e3ba2f4bb3fc88c7d60036a89a2ac4124f6a5156
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-20 11:36:19 +0330

    complet class product

--- src/main/java/controller/product/Product.java
+++ src/main/java/controller/product/Product.java
@@ -12,4 +12,70 @@ public class Product {
     private Category category;
     private List<Provider> providers;
     private Brand brand;
+
+    public Product(long id, String title, long price, String description, Category category, List<Provider> providers, Brand brand) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.providers = providers;
+        this.brand = brand;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<Provider> getProviders() {
+        return providers;
+    }
+
+    public void setProviders(List<Provider> providers) {
+        this.providers = providers;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 }
