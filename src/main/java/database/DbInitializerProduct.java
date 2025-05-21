4929c464cf024513da799853996d3f21711ee685
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-21 19:28:17 +0330

    crate class initialization product table

---
+++ src/main/java/database/DbInitializerProduct.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbInitializerProduct {
+
+    public static void initProduct() {
+        try {
+            Connection connection = DBconnection.connect();
+            Statement statement = connection.createStatement();
+
+            String createTableProduct = """
+                    CREATE TABLE IF NOT EXISTS Product (
+                        id serial PRIMARY KEY NOT NULL,
+                        title VARCHAR(100) NOT NULL UNIQUE,
+                        price BIGINT NOT NULL,
+                        description TEXT,
+                        category VARCHAR(100) NOT NULL,
+                        brand VARCHAR(100) NOT NULL,
+                        privider TEXT NOT NULL
+                        );
+                    """;
+            statement.executeUpdate(createTableProduct);
+            System.out.println("create table product");
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -2,10 +2,12 @@ package runner;
 
 import controller.Authnticate.AuthMenu;
 import database.DbInitializerAuth;
+import database.DbInitializerProduct;
 
 public class Main {
     public static void main(String[] args) {
         DbInitializerAuth.initAuth();
+        DbInitializerProduct.initProduct();
         new AuthMenu();
     }
 }
