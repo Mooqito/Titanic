af9918a4fbf073e7fe0fe32b9761ad787254d2bf
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-23 19:21:29 +0330

    show product

--- src/main/java/view/Showproduct.java
+++ src/main/java/view/Showproduct.java
@@ -12,7 +12,10 @@ public class Showproduct {
         List<Product> products = ReadAllproduct.Readproduct();
 
         for (Product p : products) {
-            System.out.println(p.getTitle());
+
+            System.out.println("product "+p.getId()+" : \n"+" Title : "+p.getTitle()+"\n price : "+p.getPrice()
+                +"\n description : "+p.getDescription()+"\n category : "+p.getCategory().getTitle()
+                    +"\n brand : "+p.getBrand().getTitle()+"\n provider : "+p.getProviders().getTitle());
         }
     }
 }
