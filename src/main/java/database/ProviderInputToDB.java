9e7ff82bc7bfd3a1c3b3402ca43dd8f867941b26
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-27 22:15:22 +0330

    Add the values ​​of the names to the database

--- src/main/java/database/ProviderInputToDB.java
+++ src/main/java/database/ProviderInputToDB.java
@@ -1,2 +1,26 @@
-package database;public class ProviderInputToDB {
+package database;
+
+import model.Product.Provider;
+
+import java.security.PrivilegedAction;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProviderInputToDB {
+
+    public static boolean providerinput (Provider provider){
+        Connection connection = DBconnection.connect();
+        String Query = "INSERT INTO Provider (title)"+"VALUES (?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(Query);
+            preparedStatement.setString(1,provider.getTitle());
+
+            preparedStatement.executeUpdate();
+            return true;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
