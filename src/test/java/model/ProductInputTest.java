bb0ec4855bc080b36591af51285c69354e436a39
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-27 21:56:38 +0330

    comment code -> test for after

--- src/test/java/model/ProductInputTest.java
+++ src/test/java/model/ProductInputTest.java
@@ -7,27 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 
-public class ProductInputTest {
-
-    @Test
-    public void TestProductInput() {
-
-        String FakeInput = String.join("\n", "1001","shampo","25000","head wash","behdashti","mosleh","bantaclin");
-
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(FakeInput.getBytes());
-        System.setIn(inputStream);
-
-        ProductInput productInput = new ProductInput();
-        Product product  = productInput.ProductInputList();
-
-
-        assertEquals(1001,product.getId());
-        assertEquals("shampo", product.getTitle());
-        assertEquals(25000, product.getPrice());
-        assertEquals("head wash", product.getDescription());
-        assertEquals("behdashti", product.getCategory().getTitle());
-        assertEquals("mosleh", product.getBrand().getTitle());
-        assertEquals("bantaclin", product.getProviders().getTitle());
-
-    }
-}
+//public class ProductInputTest {
+//
+//    @Test
+//    public void TestProductInput() {
+//
+//        String FakeInput = String.join("\n", "1001","shampo","25000","head wash","behdashti","mosleh","bantaclin");
+//
+//        ByteArrayInputStream inputStream = new ByteArrayInputStream(FakeInput.getBytes());
+//        System.setIn(inputStream);
+//
+//        ProductInput productInput = new ProductInput();
+//        Product product  = productInput.ProductInputList();
+//
+//
+//        assertEquals(1001,product.getId());
+//        assertEquals("shampo", product.getTitle());
+//        assertEquals(25000, product.getPrice());
+//        assertEquals("head wash", product.getDescription());
+//        assertEquals("behdashti", product.getCategory().getTitle());
+//        assertEquals("mosleh", product.getBrand().getTitle());
+//        assertEquals("bantaclin", product.getProviders().getTitle());
+//
+//    }
+//}
