12df75b32e45544bd1e86c06f3d5f59a9d5ed0c2
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-30 01:14:08 +0330

    Del

---
+++ src/main/java/model/Product/DeleteProduct.java
@@ -0,0 +1,27 @@
+package model.Product;
+
+import database.DBconnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteProduct {
+
+    public static boolean deleteProductById(long productId) {
+        Connection connection = DBconnection.connect();
+
+        String deleteQuery = "DELETE FROM Product WHERE id = ?";
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
+            preparedStatement.setLong(1, productId);
+
+            int affectedRows = preparedStatement.executeUpdate();
+            return affectedRows > 0; // اگر محصولی حذف شده باشد، true برمی‌گرداند
+        } catch (SQLException e) {
+            System.out.println("Error deleting product: " + e.getMessage());
+            return false;
+        }
+    }
+}
