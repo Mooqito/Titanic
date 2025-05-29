30cddc1e3c273fda92d2750163dd4332afef4e97
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-29 15:18:44 +0330

    Making a registration class and adding Gmail and applying restrictions to the username and

---
+++ src/main/java/controller/Authnticate/SingUp.java
@@ -0,0 +1,44 @@
+package controller.Authnticate;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class SingUp {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void sing_up (){
+        String username,password,confirmPassword,gmail;
+
+        while (true){
+            System.out.println("نام کاربری را وراد کنید (انگلبسی باشد)");
+            username = scanner.next();
+            if (!Pattern.matches("^[a-zA-Z0-9_]{4,}$",username)){
+                System.out.println("user error");
+            }else break;
+        }
+
+        while (true){
+            System.out.println("پسورد را وراد کنید");
+            password = scanner.next();
+
+            if (!Pattern.matches("^[a-zA-Z0-9!@#$%^&*()_+]{8,}$",password)){
+                System.out.println("password error");
+                continue;
+            }
+            System.out.println("confipass");
+            confirmPassword=scanner.next();
+            if (!password.equals(confirmPassword)){
+                System.out.println("password error");
+            }else break;
+        }
+
+        while (true){
+            System.out.println("جیمیل را وارد کنید");
+            gmail=scanner.next();
+            if (!Pattern.matches("^[a-zA-Z0-9!@#$%^&*()_+//]+@gmail\\.com$",gmail)){
+                System.out.println("gmail error");
+            }else break;
+        }
+    }
+}
