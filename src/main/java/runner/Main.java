f040d73375978450c0b5d06fbd18db555f69afd2
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-31 02:12:29 +0330

    install javafx in pom

--- src/main/java/runner/Main.java
+++ src/main/java/runner/Main.java
@@ -1,15 +1,34 @@
 package runner;
 
-import view.AuthMenu;
+import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import database.DBinitialazation.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        InitCategoryProduct.initCategory();
-        InitBrandProduct.initbrand();
-        InitProviderProduct.inintprovider();
-        DbInitializerAuth.initAuth();
-        DbInitializerProduct.initProduct();
-        new AuthMenu();
+        Application.launch(InitApp.class, args);
+    }
+
+    public static void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public static class InitApp extends Application {
+        @Override
+        public void start(Stage primaryStage) {
+            // مقداردهی اولیه دیتابیس‌ها
+            InitCategoryProduct.initCategory();
+            InitBrandProduct.initbrand();
+            InitProviderProduct.inintprovider();
+            DbInitializerAuth.initAuth();
+            DbInitializerProduct.initProduct();
+
+        }
     }
 }
