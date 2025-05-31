5938ffe1865b90e8da0f962ee07f1c75dc16eb15
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-31 13:24:41 +0330

    create dashbord

---
+++ src/main/java/view/DashboardForm.java
@@ -0,0 +1,49 @@
+package view;
+
+import javafx.geometry.NodeOrientation;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class DashboardForm {
+    private Scene scene;
+    private Stage primaryStage;
+    private VBox contentArea;
+
+    public DashboardForm(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        createDashboardScene();
+    }
+
+    private void createDashboardScene() {
+        BorderPane mainLayout = new BorderPane();
+
+        contentArea = new VBox(10);
+        contentArea.setAlignment(Pos.CENTER_RIGHT);
+        contentArea.setStyle("-fx-padding: 20;");
+        mainLayout.setCenter(contentArea);
+
+//        ManagementMenu managementMenu = new ManagementMenu(contentArea);
+
+//        MainMenu mainMenu = new MainMenu(contentArea, managementMenu);
+//        managementMenu.setMainMenu(mainMenu);
+
+        VBox leftSideMenu = new VBox(10);
+        leftSideMenu.setStyle("-fx-background-color: #f0f0f0; -fx-padding: 10;");
+        leftSideMenu.setPrefWidth(200);
+        leftSideMenu.setAlignment(Pos.TOP_RIGHT);
+//        leftSideMenu.getChildren().addAll(mainMenu, managementMenu);
+        mainLayout.setLeft(leftSideMenu);
+
+        mainLayout.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+
+        scene = new Scene(mainLayout, 1024, 768);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
