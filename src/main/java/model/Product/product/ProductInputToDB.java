3f220ae09a27bbb873496330337f0408052ad7cf
mohamad_qoreishi <mghorishi89@gmail.com>
2025-05-31 17:36:58 +0330

    Complete the registration function in the database and extract the ID value of the rest of the tibes

--- src/main/java/model/Product/product/ProductInputToDB.java
+++ src/main/java/model/Product/product/ProductInputToDB.java
@@ -18,7 +18,7 @@ public class ProductInputToDB {
                 "JOIN category c ON p.category_id = c.id " +
                 "JOIN provider pr ON p.provider_id = pr.id " +
                 "JOIN brand b ON p.brand_id = b.id " +
-                "WHERE p.name = ? AND p.price = ? AND c.name = ? AND pr.name = ? AND b.name = ?";
+                "WHERE p.title = ? AND p.price = ? AND c.title = ? AND pr.title = ? AND b.title = ?";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(Query);
 
@@ -39,25 +39,25 @@ public class ProductInputToDB {
     }
 
 
-    public static boolean updateProductQuantity (String name, long price, String category, String provider, String brand, int additionalQuantity){
+    public static boolean updateProductQuantity (Product product){
         Connection connection = DBconnection.connect();
 
         String Query = "UPDATE product SET Quantity = Quantity + ? " +
                 "WHERE id IN (SELECT p.id FROM product p " +
                 "JOIN category c ON p.category_id = c.id " +
-                "OIN provider pr ON p.provider_id = pr.id " +
+                "JOIN provider pr ON p.provider_id = pr.id " +
                 "JOIN brand b ON p.brand_id = b.id " +
-                "WHERE p.name = ? AND p.price = ? AND c.name = ? AND pr.name = ? AND b.name = ?)";
+                "WHERE p.title = ? AND p.price = ? AND c.title = ? AND pr.title = ? AND b.title = ?)";
 
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(Query);
 
-            preparedStatement.setLong(1,additionalQuantity);
-            preparedStatement.setString(2,name);
-            preparedStatement.setLong(3,price);
-            preparedStatement.setString(4,category);
-            preparedStatement.setString(5,provider);
-            preparedStatement.setString(6,brand);
+            preparedStatement.setLong(1,product.getQuantity());
+            preparedStatement.setString(2,product.getTitle());
+            preparedStatement.setLong(3,product.getPrice());
+            preparedStatement.setString(4,product.getCategoryTitle());
+            preparedStatement.setString(5,product.getProviderTitle());
+            preparedStatement.setString(6,product.getBrandTitle());
 
             return preparedStatement.executeUpdate() > 0;
 
@@ -70,19 +70,25 @@ public class ProductInputToDB {
 
     public static boolean productInput(Product product) {
 
+
+
         Connection connection = DBconnection.connect();
 
         String Query = "INSERT INTO product (title,price,description,category_id,brand_id,provider_id,Quantity)" + "VALUES (?,?,?,?,?,?,?)";
 
         try {
+            int category_Id = getOrCreateId(connection, "category", product.getCategoryTitle());
+            int provider_Id = getOrCreateId(connection, "provider", product.getProviderTitle());
+            int brand_Id = getOrCreateId(connection, "brand",product.getBrandTitle());
+
             PreparedStatement preparedStatement = connection.prepareStatement(Query);
 
             preparedStatement.setString(1,product.getTitle());
             preparedStatement.setLong(2,product.getPrice());
             preparedStatement.setString(3,product.getDescription());
-            preparedStatement.setLong(4,product.getCategory_id());
-            preparedStatement.setLong(5,product.getBrand_id());
-            preparedStatement.setLong(6,product.getProviders_id());
+            preparedStatement.setLong(4,category_Id);
+            preparedStatement.setLong(5,provider_Id);
+            preparedStatement.setLong(6,brand_Id);
             preparedStatement.setLong(7,product.getQuantity());
 
             preparedStatement.executeUpdate();
@@ -91,6 +97,16 @@ public class ProductInputToDB {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    private static int getOrCreateId(Connection conn, String table, String name) throws SQLException {
+        PreparedStatement selectStmt = conn.prepareStatement(
+                "SELECT id FROM " + table + " WHERE name = ?"
+        );
+        selectStmt.setString(1, name);
+        ResultSet rs = selectStmt.executeQuery();
 
+        if (rs.next()) {
+            return rs.getInt("id");
+        }throw new SQLException("Failed to create new " + table + " entry");
     }
 }
