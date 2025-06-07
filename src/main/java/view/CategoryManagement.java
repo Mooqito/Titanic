dafefe2811ef8a13d452d70e353b9fd04e3d5870
mohamad_qoreishi <mghorishi89@gmail.com>
2025-06-07 18:51:35 +0330

    Show list category and product

--- src/main/java/view/CategoryManagement.java
+++ src/main/java/view/CategoryManagement.java
@@ -20,6 +20,7 @@ import model.Product.product.Product;
 import model.Product.product.ReadAllproduct;
 import runner.Main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,6 +37,7 @@ public class CategoryManagement extends VBox{
         setManaged(false);
         setStyle("-fx-padding:0 20 0 0;");
         setAlignment(Pos.TOP_RIGHT);
+        createCategoryMenu();
     }
 
     private void createCategoryMenu() {
@@ -68,7 +70,7 @@ public class CategoryManagement extends VBox{
         if(title.equals("افزودن و حذف دسته بندی")) {
             content.getChildren().add(createAddDeleteCategoryForm());
         } else if (title.equals("نمایش دسته بندی")) {
-//            content.getChildren().add(createAddDeleteCategoryForm());
+            content.getChildren().add(createShowCategoryForm());
         }
 
         Button backBut = new Button("بازگشت");
@@ -156,12 +158,102 @@ public class CategoryManagement extends VBox{
         return form;
     }
     private void updateCategoryList(ListView<String> listView) {
-        List<Brand> brands = GetAllBrand.getAllBrand();
+        List<Category> category = GetAllCategory.getAllCategory();
         ObservableList<String> brandNames = FXCollections.observableArrayList(
-                brands.stream()
-                        .map(Brand::getTitle)
+                category.stream()
+                        .map(Category::getTitle)
                         .collect(Collectors.toList())
         );
         listView.setItems(brandNames);
     }
-}
+
+    private VBox createShowCategoryForm() {
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefViewportHeight(500);
+
+        VBox form = new VBox();
+        form.setAlignment(Pos.TOP_RIGHT);
+        form.setPadding(new Insets(10));
+
+        List<Category> categories = GetAllCategory.getAllCategory();
+        List<Product> products = ReadAllproduct.Readproduct();
+
+        for (Category category : categories){
+            TitledPane titledPane = new TitledPane();
+            titledPane.setText(category.getTitle());
+
+            List<Product> categoryPruduct =  products.stream()
+                    .filter(product -> product.getBrandTitle().equals(category.getTitle()))
+                    .collect(Collectors.toList());
+
+            if (categoryPruduct.isEmpty()){
+                Label emptyLabel = new Label("هیچ محصولی در این برند وجود ندارد");
+                titledPane.setContent(emptyLabel);
+            } else {
+                TableView<Product> table = new TableView<>();
+                table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+                TableColumn<Product, String> titleCol = new TableColumn<>("نام محصول");
+                titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+                titleCol.setPrefWidth(150);// ستون قیمت
+                TableColumn<Product, Long> priceCol = new TableColumn<>("قیمت");
+                priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+                priceCol.setPrefWidth(100);
+                priceCol.setCellFactory(column -> new TableCell<Product, Long>() {
+                    @Override
+                    protected void updateItem(Long price, boolean empty) {
+                        super.updateItem(price, empty);
+                        if (empty || price == null) {
+                            setText(null);
+                        } else {
+                            setText(String.format("%,d", price));
+                        }
+                    }
+                });
+                TableColumn<Product, Long> quantityCol = new TableColumn<>("موجودی");
+                quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+                quantityCol.setPrefWidth(80);
+
+                TableColumn<Product, String> brandCol = new TableColumn<>("برنذ ");
+                brandCol.setCellValueFactory(new PropertyValueFactory<>("brandTitle"));
+                brandCol.setPrefWidth(120);
+
+                TableColumn<Product, String> descriptionCol = new TableColumn<>("توضیحات");
+                descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+                descriptionCol.setPrefWidth(200);
+
+                TableColumn<Product, String> providerCol = new TableColumn<>("تامین‌کننده");
+                providerCol.setCellValueFactory(new PropertyValueFactory<>("providerTitle"));
+                providerCol.setPrefWidth(120);
+
+                titleCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+                priceCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+                quantityCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+                brandCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+                descriptionCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+                providerCol.setStyle("-fx-alignment: CENTER-RIGHT;");
+
+                table.getColumns().addAll(
+                        titleCol, priceCol, quantityCol, brandCol,
+                        providerCol, descriptionCol
+                );
+
+                table.setItems(FXCollections.observableArrayList(categoryPruduct));
+
+                table.setPrefHeight(Math.min(categoryPruduct.size() * 30 + 40, 200));
+
+                titledPane.setContent(table);
+            }
+
+            form.getChildren().add(titledPane);
+        }
+
+        scrollPane.setContent(form);
+
+        VBox mainContainer = new VBox(scrollPane);
+        mainContainer.setPadding(new Insets(10));
+
+        return mainContainer;
+        }
+    }
