cda69dd78f197cd9f52ec529c3bd5e02d49594b8
mohamad_qoreishi <mghorishi89@gmail.com>
2025-06-09 10:39:00 +0330

    Define order fields

---
+++ src/main/java/controller/Order/Order.java
@@ -0,0 +1,13 @@
+package controller.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+    private int id;
+    private int customerId;
+    private LocalDateTime orderDate;
+    private double totalAmount;
+//    private List<or>
+
+}
